fa58e2aabb96b77d84e610ca16bf4953ecc0c5ed
HereticalWriter <162382029+HereticalWriter@users.noreply.github.com>
2024-04-13 03:50:48 +0200

    Upgrade code with objects

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
+function Mutator(name, page) {
+    this.name = name;
+    this.page = page;
+    this.url = `icons/${this.name.replaceAll(" ","").toLowerCase()}.png`
+}
+
 let mutatorList = [
-    "Walking Infested (page 1)",
-    "Outbreak (page 1)",
-    "Time Warp (page 1)",
-    "Speed Freaks (page 1)",
-    "Barrier (page 1)",
-    "Avenger (page 1)",
-    "Evasive Maneuvers (page 1)",
-    "Scorched Earth (page 1)",
-    "Alien Incubation (page 2)",
-    "Long Range (page 2)",
-    "Concussive Attacks (page 2)",
-    "Just Die! (page 2)",
-    "Temporal Field (page 2)",
-    "Photon Overload (page 3)",
-    "Life Leech (page 3)",
-    "Power Overwhelming (page 3)",
-    "Transmutaion (page 3)",
-    "Diffusion (page 3)",
-    "Eminent Domain (page 3)",
-    "Inspiration (page 4)",
-    "Hardened Will (page 4)"
+    WalInf = new Mutator("Walking Infested", 1),
+    Outbre = new Mutator("Outbreak", 1),
+    TimWar = new Mutator("Time Warp", 1),
+    SpeFre = new Mutator("Speed Freaks", 1),
+    Barrie = new Mutator("Barrier", 1),
+    Avenge = new Mutator("Avenger", 1),
+    EvaMan = new Mutator("Evasive Maneuvers", 1),
+    ScoEar = new Mutator("Scorched Earth", 1),
+    AliInc = new Mutator("Alien Incubation", 2),
+    LonRan = new Mutator("Long Range", 2),
+    ConAtt = new Mutator("Concussive Attacks", 2),
+    JusDie = new Mutator("Just Die!", 2),
+    TemFie = new Mutator("Temporal Field", 2),
+    PhoOve = new Mutator("Photon Overload", 3),
+    LifLee = new Mutator("Life Leech", 3),
+    PowOve = new Mutator("Power Overwhelming", 3),
+    Transm = new Mutator("Transmutation", 3),
+    Diffus = new Mutator("Diffusion", 3),
+    EmiDom = new Mutator("Eminent Domain", 3),
+    Inspir = new Mutator("Inspiration", 4),
+    HarWil = new Mutator("Hardened Will", 4)
 ];
 
-function getMutator() {
+function getMutatorIndex() {
     return Math.floor(Math.random() * mutatorList.length);
 }
 
 const body = document.querySelector("body");
 const btn = document.querySelector("button");
 
-let used = [];
 btn.addEventListener("click", () => {
-    let newMutator = document.createElement("p");
-    let number = getMutator();
-    while (used.includes(number)) {
-        number = getMutator();
-    }
-    used.push(number);
 
-    newMutator.textContent = mutatorList[number];
-    body.appendChild(newMutator);
+    if (mutatorList.length === 0) {
+        console.log("end of list");
+        return;
+    }
+
+    let mutatorCard = document.createElement("p");
+    let mutatorIndex = getMutatorIndex();
+
+    mutatorCard.textContent = `${mutatorList[mutatorIndex].name}, page ${mutatorList[mutatorIndex].page}`;
+    body.appendChild(mutatorCard);
+
+    let mutatorIcon = document.createElement("img");
+    mutatorIcon.setAttribute("src", `${mutatorList[mutatorIndex].url}`);
+    mutatorIcon.setAttribute("alt", `${mutatorList[mutatorIndex].name}`);
+    mutatorCard.appendChild(mutatorIcon);
+
+    mutatorList.splice(mutatorIndex, 1);
 })
